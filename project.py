99518cc5877814f42d16bee6a51034f745c84df0
REMELH01 <rmelhornmusic@gmail.com>
2022-10-04 19:13:09 -0400

    Added all nail polishes owned and organized by type

---
+++ project.py
@@ -0,0 +1,19 @@
+import random
+
+nail_base = ('Smoothing', 'Peely', 'Long Lasting')
+
+nail_creme = ('Royal-Tea Blue', 'Matcha Maker', "I'm over Brew", "See Y'all Later Chai", 'Bring Me the Teal', 'High Tea Hibiscus', 'Butterscotch Hop', 'One-Coat Black', 'Not Milky White', 'Indigo Away', 'One Coat Black', 'Cyantific', 'What Do You Pink?')
+
+nail_metalics = ('Gift Reciept', 'Cold Shoulder', 'Cheap Champagne', 'Mint Money', 'Fake Date')
+
+nail_holo_linear = ('Blue Freezie', 'Purple Slushie', 'Magenta Jelly', 'Red Licorice', 'Orange Drink', 'Lemon Sucker', 'Green Taffy')
+
+nail_holo_glitter = ('Zyler the Cat', 'Menchie the Cat', 'Everything is Pine', 'Choco Holo', 'Naughty List', 'Gold Play Button', 'Black Holo Wish', 'Plumb Luck', 'Party Punch', 'Midnight Spark', 'Rainbow Snow', 'Play Rose', 'Frost Light')
+
+nail_multichrome = ('Chameleon Coat', "Blue Ain't Slick", 'Missed-Shift', 'Purple with Envy', "Cats' Evation")
+
+nail_shimmer = ('Got Cake')
+
+nail_toppers = ('Aurora Unicorn Skin', 'Solar Unicorn Skin', 'Cosmic Unicorn Skin', 'Flakie Holo Taco', 'Linear Holo Taco', 'Scattered Holo Taco', 'Sonic Unicorn Skin', 'Galatic Unicorn Skin', 'Lunar Unicorn Skin')
+
+nail_finishes = ('Glossy Taco', 'Super Glossy Taco', 'Matte Taco')
