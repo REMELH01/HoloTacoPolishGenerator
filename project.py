7c66ae1895e0904b1424390f83ad8a0f8de1c32d
REMELH01 <rmelhornmusic@gmail.com>
2022-10-17 19:12:05 -0400

    Changing to make it interactive. Stopping at Metalics with Syntax Error. Ask Mentors about error

--- project.py
+++ project.py
@@ -2,31 +2,39 @@ import random
 
 import pandas as pd
 
-nail_base = ('Smoothing', 'Peely', 'Long Lasting')
+# Changing to make interactive with inputs.
 
-nail_creme = ('Royal-Tea Blue', 'Matcha Maker', "I'm over Brew", "See Y'all Later Chai", 'Bring Me the Teal', 'High Tea Hibiscus', 'Butterscotch Hop', 'One-Coat Black', 'Not Milky White', 'Indigo Away', 'One Coat Black', 'Cyantific', 'What Do You Pink?')
+nail_base = ('Smoothing Base', 'Peely Base', 'Long Lasting Base')
+
+# Boxes will be categorized as 'Metalics', 'Rainbow Holo', or 'Multichrome'.
 
 nail_metalics = ('Gift Reciept', 'Cold Shoulder', 'Cheap Champagne', 'Mint Money', 'Fake Date')
 
-nail_holo_linear = ('Blue Freezie', 'Purple Slushie', 'Magenta Jelly', 'Red Licorice', 'Orange Drink', 'Lemon Sucker', 'Green Taffy')
-
-nail_holo_glitter = ('Zyler the Cat', 'Menchie the Cat', 'Everything is Pine', 'Choco Holo', 'Naughty List', 'Gold Play Button', 'Black Holo Wish', 'Plumb Luck', 'Party Punch', 'Midnight Spark', 'Rainbow Snow', 'Play Rose', 'Frost Light')
+nail_rainbow_holo = ('Blue Freezie', 'Purple Slushie', 'Magenta Jelly', 'Red Licorice', 'Orange Drink', 'Lemon Sucker', 'Green Taffy')
 
 nail_multichrome = ('Chameleon Coat', "Blue Ain't Slick", 'Missed-Shift', 'Purple with Envy', "Cats' Evation")
-
-nail_shimmer = ('Got Cake')
 
 nail_toppers = ('Aurora Unicorn Skin', 'Solar Unicorn Skin', 'Cosmic Unicorn Skin', 'Flakie Holo Taco', 'Linear Holo Taco', 'Scattered Holo Taco', 'Sonic Unicorn Skin', 'Galatic Unicorn Skin', 'Lunar Unicorn Skin')
 
 nail_finishes = ('Glossy Taco', 'Super Glossy Taco', 'Matte Taco')
 
-polish_types = [nail_creme, nail_holo_glitter, nail_holo_linear, nail_metalics, nail_multichrome, nail_shimmer]
+polish_types = [nail_rainbow_holo, nail_metalics, nail_multichrome]
 
-polish_types2 = {"nail_creme": nail_creme, "nail_holo_glitter": nail_holo_glitter, "nail_metalics": nail_metalics}
+#start program
 
-def choose_polish():
-    polish_type = random.choice(polish_types)
-    single_polish = random.choice(polish_type)
-    return single_polish
+print('Hello! We will be using Holo Taco nail polishes for this program.')
 
-df = pd.DataFrame(polish_types2)
+def choose_base_polish(): 
+
+    nail_base_random = random.choice(nail_base) 
+
+nail_base_choice = 'First, start with a base of ' + str(choose_base_polish) + '.'
+
+main_polish = (input('What main polish box would you like?'))
+
+def choose_main_metalics():
+    nail_metalics_random = random.choice(nail_metalics)
+
+if nail_metalics == 'Metalics'
+    metalics_sentence = 'Okay, please use ' + choose_main_metalics +'.'
+    print(metalics_sentence)
